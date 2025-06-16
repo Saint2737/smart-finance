b2637f6be84c2263718d916016a0fbd88098df46
Saint2737 <165212573+Saint2737@users.noreply.github.com>
2025-06-16 12:25:04 +0300

    Update CreditDecision.java

--- CreditDecision/src/CreditDecision.java
+++ CreditDecision/src/CreditDecision.java
@@ -32,24 +32,29 @@ public class CreditDecision {
             double halfRevenue = totalRevenue / 2;
 
             if (totalDebt < halfRevenue) {
-                return Debt_worhty;
+                return "Debt_worthy";
                 System.out.println("You have a good capacity for debt");
                 System.out.println("You can take on more debt ");
                 System.out.println("Your debt to income ratio is 1:2 ");
 
-            } else if (totalDebt > halfRevenue) {
-                return Debt_unworthy;
+            } else if (totalDebt > halfRevenue && totalDebt < totalRevenue) {
+                return "Debt_unworthy";
                 System.out.println("You have a limited capacity for debt");
                 System.out.println("You can take on more debt but cautiously; or as a last resort ");
                 System.out.println("Your debt to income ratio is 1:1.5 ");
 
             } else if (totalDebt > totalRevenue) {
-                return Debt_Unworthy;
+                return "Debt_Unworthy";
                 System.out.println("You have no capacity for debt");
                 System.out.println("You can take up any more debt");
                 System.out.println("Your debt to income ratio is 2:1 ");
 
-            }
+            } else{
+                    return "Debt_Unworthy";
+                    System.out.println("You have no capacity for debt");
+                    System.out.println("You total debt is equal to your total revenue");
+                    
+        
         }
     }
 }
